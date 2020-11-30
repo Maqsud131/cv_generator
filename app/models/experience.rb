a352a1d70cf138ba9ba44d334381092f7268553c
Maqsud <maqsud13101033@gmail.com>
2020-11-30 21:18:42 +0600

    Fixed edit page and few other bugs

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,5 +1,5 @@
 class Experience < ApplicationRecord
     belongs_to :cv
-    has_many :text_lines
-    accepts_nested_attributes_for :text_lines
+    has_many :text_lines, inverse_of: :experience, dependent: :destroy
+    accepts_nested_attributes_for :text_lines, reject_if: :all_blank, allow_destroy: true
 end
